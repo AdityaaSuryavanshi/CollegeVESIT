5574a25e8a8d63df6d7a01090c50c04c567ac5ef
Cargame <149827976+AdityaaSuryavanshi@users.noreply.github.com>
2023-11-05 10:56:33 +0530

    main.c

--- cargameFinal/Cargithub/main.c
+++ cargameFinal/Cargithub/main.c
@@ -4,89 +4,60 @@
 #include <unistd.h>
 
 int height=21,width=28;
-int x=21,y=14,redo,multiplayer=0,randomizer=0,obstacleX=3,obstacleY=14,flag=0,score=0,gameEnd=0,flag2,n;
-float slepn=0.99;
+int carx=21,cary=14,redo,selection=0,obstacleX=3,obstacleY=14,flag=0,score=0,gameEnd=0,flag2,n;
+float slepn=1;
+
 
 char car(int coord)
 {
     int m,n,p,q;
     printf("_\n");
-    for(m=0;m<=coord+1;m++)
-    {
-        if(m==coord-1)
-        {
-            printf("/");
-        }
-        else if(m==coord+1)
-        {
-            printf("\\ \n");
-        }
-        else
-        {
-            printf(" ");
-        }
-    }
-    for(n=0;n<=coord+2;n++)
-    {
-        if(n==coord-2)
-        {
-            printf("|");
-        }
-        else if(n==coord)
-        {
-            printf("o");
-        }
-        else if(n==coord+2)
-        {
-            printf("| \n");
-        }
-        else
-        {
-            printf(" ");
-        }
-        }
-    for(p=0;p<=coord+1;p++)
-    {
-        if(p==coord-1)
-        {
-            printf("\\");
-            }
-        else if(p==coord+1)
-        {
-            printf("/\n");
-        }
-        else
-        {
-            printf(" ");
-        }
-        }
-    for(q=0;q<=coord;q++)
-    {
-        if(q==coord)
-        {
-        printf("v");
-        }
+    for(m=0;m<=coord+1;m++){
+        if(m==coord-1){
+            printf("/");}
+        else if(m==coord+1){
+            printf("\\ \n");}
         else{
-            printf(" ");
+            printf(" ");}
+    }
+    for(n=0;n<=coord+2;n++){
+        if(n==coord-2){
+            printf("|");}
+        else if(n==coord){
+            printf("o");}
+        else if(n==coord+2){
+            printf("| \n");}
+        else{
+            printf(" ");}
         }
+    for(p=0;p<=coord+1;p++){
+        if(p==coord-1){
+            printf("\\");}
+        else if(p==coord+1){
+            printf("/\n");}
+        else{
+            printf(" ");}
+        }
+    for(q=0;q<=coord;q++){
+        if(q==coord){
+        printf("v");}
+        else{
+            printf(" ");}
     }
     return 0;
 }
 
-int select(int a)
-{
-        switch(a)
-        {
-            case 2:
-            {
-                multiplayer=1;
-                return multiplayer;
+
+int select(int a){
+        switch(a){
+            case 1:{
+                selection=1;
+                return selection;
                 break;
             }
-            case 1:
-            {
-                randomizer=1;
-                return randomizer;
+            case 2:{
+                selection=2;
+                return selection;
                 break;
             }
         }
@@ -94,29 +65,52 @@ int select(int a)
 
 void display()
 {
-    printf("Hello! Welcome to SPEED FOR NEED.\nWe hope you enjoy it!\n\n");
+    printf("Hi Welcome to SPEED FOR NEED.\nDrive safe!\n\n");
     sleep(1);
     int a;
-    printf("Please select which mode you would like to play the game in\n1.Single Player\n2.Multiplayer\n");
+    printf("Select the mode you'd like to play (Enter serial number)\n1.Single player\n2.Multiplayer\n");
     scanf("%d",&a);
     select(a);
     sleep(1);
-    if(randomizer==1)
-    {
+    if(selection==1){
         system("cls");
-        printf("The controls of the game are \n Player control:-\n A = Left \n D = Right\n\nWARNING:- GAME SPEED INCREASES AFTER THE FIRST LOOP\n");
+        printf("Game controls are as follows \n Player control:-\n A = Left \n D = Right");
         sleep(2);
+        printf("\n\nThe game ends when your car collides with the other.");
+        sleep(1);
+        printf("\n\nWARNING:-WATCH OUT FOR SPEEDING");
+        sleep(1);
+        printf("\n\nGame starts in  3");
+        sleep(1);
+        printf("  2");
+        sleep(1);
+        printf("  1");
+        sleep(1);
+        printf("   GO");
     }
-    else if(multiplayer==1)
-    {
+    else if(selection==2){
         system("cls");
-        printf("The controls of the game are \n Player 1:-\n A = Left \n D = Right  \n\n\n FOR MULTIPLAYER \n J = Left  \n L = Right \n\nWARNING:- GAME SPEED INCREASES AFTER THE FIRST LOOP\n");
+        printf("Game controls are follows \n Player 1:-\n A = Left \n D = Right  \n\n Player 2:-\n J = Left  \n L = Right \n");
         sleep(2);
+        printf("\n Instructions-\n");
+        sleep(2);
+        printf("1.Player 1 controls the primary car. Player 1 wins if he can make it to 200 points without colliding with player 2.");
+        sleep(3);
+        printf("\n\n 2.Player 2 controls the second car. Player 2 wins if he can bring player 1 to a collision before the latter reaches 200 points.");
+        sleep(3);
+        printf("\n\n WARNING:-WATCH OUT FOR SPEEDING\n");
+        sleep(5);
+        printf("\nGame starts in  3");
+        sleep(1);
+        printf("  2");
+        sleep(1);
+        printf("  1");
+        sleep(1);
+        printf("   GO");
     }
 }
 
-void track()
-{
+void track(){
     system("cls");
     int i,j;
     for(i=0;i<=height;i++){
@@ -125,8 +119,8 @@ void track()
                 printf("|");
             }
             else{
-                if(i==x && j==y){
-                    car(y);
+                if(i==carx && j==cary){
+                    car(cary);
                 }
                 else if(i==obstacleX && j==obstacleY){
                     car(obstacleY);
@@ -139,8 +133,8 @@ void track()
         printf("\n");
     }
     for(n=0;n<=width;n++){
-        printf("_");
-    }
+        printf("_");}
+    printf("score:%d",score);
 }
 
 void input()
@@ -150,14 +144,12 @@ void input()
         switch(getch())
         {
             case 'a':{
-                if(y>4){
-                    y-=10;
-                    }
+                if(cary>4){
+                    cary-=10;}
                 break;}
             case 'd':{
-                if(y<24){
-                    y+=10;
-                }
+                if(cary<24){
+                    cary+=10;}
                 break;
             }
         }
@@ -190,13 +182,13 @@ void multi(){
     if(kbhit()){
         switch(getch()){
             case 'a':{
-                if(y>4){
-                    y-=10;}
+                if(cary>4){
+                    cary-=10;}
                 break;
             }
             case 'd':{
-                if(y<24){
-                    y+=10;}
+                if(cary<24){
+                    cary+=10;}
                 break;
             }
             case 'j':{
@@ -214,113 +206,98 @@ void multi(){
     }
 }
 
-void loop()
-{
-    if(obstacleX>21)
-    {
+void loop(){
+    if(obstacleX>21){
         random();
         obstacleX=0;
         score+=10;
-        slepn=0.9;
+        slepn=0.99;
     }
-    obstacleX++;
-}
+    obstacleX++;}
+
 
 void multiloop(){
-    if(obstacleX>21)
-        {
+    if(obstacleX>21){
         obstacleX=0;
         score+=10;
         slepn=0.9;
-        if(score==200)
-        {
-            gameEnd=1;
+        if(score==200){
+            gameEnd=1;}
         }
-        }
-    obstacleX++;
-}
+    obstacleX++;}
+
+
 
 void end(){
-    if(y==obstacleY && x==obstacleX){
+    if(cary==obstacleY && carx==obstacleX){
         gameEnd=1;}
 }
 
-int endDisplay()
-{
-    if(score>=300)
-    {
-        printf("\nYour score is %d!\n\nYou have achieved one of the highest scores!Congrats!!\n",score);
+int endDisplay(){
+    if(score>=300){
+        printf("\n%d! That's a good score!\n",score);
     }
     else
     {
-        printf("Your score %d!\n\nYou did great!\n",score);
+        printf("\nGood job!\n");
     }
-    printf("Do you wish to continue the game?\n1.Retry\n2.Exit\n");
+    printf("Do you wish to continue the game?(Enter serial number)\n1.Retry\n2.Exit\n");
     scanf("%d",&redo);
     return redo;
 }
 
 
 int endDisplay2(){
-    if(score>=200)
-    {
-        printf("Your score:%d\n No collision occured! Player 1,victory is yours.\n",score);
+    if(score>=200){
+        printf("Your score:%d \nNo collision occured!Player 1, Victory is yours.\n",score);
     }
     else
     {
-        printf("The cars collided!Player 2,victory is yours.\n");
+        printf("\nThe cars collided!Player 2, Victory is yours.\n");
     }
-    printf("Do you wish to continue the game?\n1.Retry\n2.Exit\n");
+    printf("Do you wish to continue the game?(Insert serial number)\n1.Retry\n2.Exit\n");
     scanf("%d",&redo);
     return redo;
 }
 
-int main()
-{
+int main(){
     display();
-    if(randomizer==1)
-    {
-        while(gameEnd!=1)
-    {
-
+    if(selection==1){
+        while(gameEnd!=1){
         sleep(slepn);
         input();
         track();
         end();
         loop();
-        if(gameEnd==1)
-        {
+        if(gameEnd==1){
             endDisplay();
-            if(redo==1)
-            {
+            if(redo==1){
                 score=0;
                 gameEnd=0;
+                slepn=1;
             }
         }
     }
     }
-    else if(multiplayer==1){
+    else if(selection==2){
         while(gameEnd!=1){
         sleep(slepn);
         multi();
         track();
         end();
         multiloop();
-        if(gameEnd==1)
-        {
+        if(gameEnd==1){
             endDisplay2();
-            if(redo==1)
-            {
+            if(redo==1){
                 score=0;
                 gameEnd=0;
-            }
+                slepn=1;}
         }
     }
     }
-    if(gameEnd==1)
-    {
+    if(gameEnd==1){
         system("cls");
-        printf("Thank you for playing\n");
+        printf("Thank you for driving with us!\n");
     }
     return 0;
 }
